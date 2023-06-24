89af840f05f4e72a4a6ef5a469ea991cfe306a88
Gabriel Borkowski <gabriell.borkowski@gmail.com>
2023-06-24 23:56:15 +0200

    OrderController first implementation + OrderDto

---
+++ src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
@@ -0,0 +1,18 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class OrderDto {
+    private Long id;
+    private String name;
+    private Long cartId;
+    private Long userId;
+    private LocalDate created;
+    private BigDecimal cost;
+}
