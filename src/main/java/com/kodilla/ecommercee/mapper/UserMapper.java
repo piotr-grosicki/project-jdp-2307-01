73ecc0728ab6d62aa1ce800af7432f3bda305acd
Gabriel Borkowski <gabriell.borkowski@gmail.com>
2023-07-10 21:56:12 +0200

    UserController first method + UserMapper + UserDbService

---
+++ src/main/java/com/kodilla/ecommercee/mapper/UserMapper.java
@@ -0,0 +1,34 @@
+package com.kodilla.ecommercee.mapper;
+
+import com.kodilla.ecommercee.domain.User;
+import com.kodilla.ecommercee.domain.UserDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserMapper {
+    public User mapToUser(final UserDto userDto){
+        return new User(
+                userDto.getUserId(),
+                userDto.getFirstName(),
+                userDto.getLastName(),
+                userDto.getAddress(),
+                userDto.getUserKey(),
+                userDto.getIsActive(),
+                userDto.getCarts(),
+                userDto.getOrders()
+        );
+    }
+
+    public UserDto mapToUserDto(final User user){
+        return new UserDto(
+                user.getId(),
+                user.getFirstname(),
+                user.getLastname(),
+                user.getAddress(),
+                user.getUserKey(),
+                user.getIsActive(),
+                user.getCarts(),
+                user.getOrders()
+        );
+    }
+}
