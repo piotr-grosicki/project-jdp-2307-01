97a683fbea37f5eb5d24bb8d66b82d2fa253ca6c
Aleksander Orlowski <alek.j.orlowski@gmail.com>
2023-07-16 01:14:37 +0200

    edit: Controller, mapper, service / product - added constructor

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -3,16 +3,15 @@ package com.kodilla.ecommercee.controller;
 import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.domain.ProductDto;
 import com.kodilla.ecommercee.domain.ProductNotFoundException;
-import com.kodilla.ecommercee.exception.GroupNotFoundException;
+import com.kodilla.ecommercee.exception.GroupProductNotFoundException;
 import com.kodilla.ecommercee.mapper.ProductMapper;
-import com.kodilla.ecommercee.service.ProductService;
+import com.kodilla.ecommercee.service.ProductDbService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/v1/products")
@@ -20,58 +19,34 @@ import java.util.List;
 public class ProductController {
 
     private final ProductMapper productMapper;
-    private final ProductService productService;
+    private final ProductDbService productService;
 
-    @GetMapping
-    public ResponseEntity<List<ProductDto>> getProducts() {
-        List<Product> products = productService.getAllProducts();
-        List<ProductDto> productDtos = productMapper.mapToProductDtoList(products);
-        return ResponseEntity.ok(productDtos);
-    }
-
-    @GetMapping("/{productId}")
-    public ResponseEntity<?> getProduct(@PathVariable Long productId) {
-        try {
-            Product product = productService.getProductById(productId);
-            ProductDto productDto = productMapper.mapToProductDto(product);
-            return ResponseEntity.ok(productDto);
-        } catch (ProductNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product with given id doesn't exist :(");
-        }
-    }
-
-
-    @DeleteMapping("/{productId}")
-    public ResponseEntity<?> deleteProduct(@PathVariable Long productId) {
-        try {
-            productService.deleteProductById(productId);
-            return ResponseEntity.ok().build();
-        } catch (ProductNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product with given id doesn't exist :(");
-        }
-    }
-
-    @PostMapping
-    public ResponseEntity<Void> createProduct(@RequestBody ProductDto productDto) throws GroupNotFoundException {
-        Product product = productMapper.mapToProduct(productDto);
-        productService.saveProduct(product);
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createProduct(@RequestBody ProductDto productsDto) throws GroupProductNotFoundException {
+        productService.saveProduct(productMapper.mapToProduct(productsDto));
         return ResponseEntity.ok().build();
     }
-    @PutMapping("/{productId}")
-    public ResponseEntity<?> updateProduct(@PathVariable Long productId, @RequestBody ProductDto productDto) {
-        try {
-            Product existingProduct = productService.getProductById(productId);
-            Product updatedProduct = productMapper.mapToProduct(productDto);
-            existingProduct.setName(updatedProduct.getName());
-            existingProduct.setDescription(updatedProduct.getDescription());
-            existingProduct.setPrice(updatedProduct.getPrice());
-            existingProduct.setGroupProduct(updatedProduct.getGroupProduct());
-            productService.saveProduct(existingProduct);
-            return ResponseEntity.ok().build();
-        } catch (ProductNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product with given id doesn't exist :(");
-        }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> getProducts(){
+        return ResponseEntity.ok(productMapper.mapToProductDtoList(productService.getAllProducts()));
     }
 
-}
+    @GetMapping(value = "{productsId}")
+    public ResponseEntity<ProductDto> getProduct(@PathVariable Long productsId) throws  ProductNotFoundException{
+        return ResponseEntity.ok(productMapper.mapToProductDto(productService.getProduct(productsId).orElseThrow(() -> new ProductNotFoundException(productsId))));
+    }
 
+    @DeleteMapping(value = "{productsId}")
+    public ResponseEntity<Void> deleteProducts(@PathVariable Long productsId){
+        productService.deleteProduct(productsId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ProductDto> updateProduct(@RequestBody ProductDto productsDto) throws GroupProductNotFoundException, ProductNotFoundException{
+        Product product = productMapper.mapToProduct(productsDto);
+        Product savedProduct = productService.saveProduct(product);
+        return ResponseEntity.ok(productMapper.mapToProductDto(savedProduct));
+    }
+}
---
+++ src/main/java/com/kodilla/ecommercee/service/ProductDbService.java
@@ -0,0 +1,38 @@
+package com.kodilla.ecommercee.service;
+
+import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.domain.ProductNotFoundException;
+import com.kodilla.ecommercee.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class ProductDbService {
+   @Autowired
+   private final ProductRepository productRepository;
+
+        public Product saveProduct(final Product product) {
+        return productRepository.save(product);
+    }
+
+    public Optional<Product> getProduct(Long id) throws ProductNotFoundException {
+        Optional<Product> result = Optional.ofNullable(productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id)));
+        return result;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public void deleteProduct(final Long id) {
+        productRepository.deleteById(id);
+    }
+}
+
+
+
