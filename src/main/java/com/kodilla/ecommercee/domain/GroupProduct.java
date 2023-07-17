b3d695d58115ea6cf88207889c31305be7b99b44
Michal <terox@wp.pl>
2023-07-17 20:17:16 +0200

    JDP230701-22-fix

--- src/main/java/com/kodilla/ecommercee/domain/GroupProduct.java
+++ src/main/java/com/kodilla/ecommercee/domain/GroupProduct.java
@@ -1,6 +1,7 @@
 package com.kodilla.ecommercee.domain;
 
 import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
@@ -10,6 +11,7 @@ import java.util.List;
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "PRODUCT_GROUPS")
 public class GroupProduct {
 
